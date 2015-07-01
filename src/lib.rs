e8e4b377e2881069d35a3c0708f74cbe1ae566d6
Zachary Bush <zachary.bush@meraki.net>
2015-06-30 20:40:04 -0700

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,31 @@
+//! A simple vault for storing encrypted data
+//!
+//! # Usage
+//!
+//! ```
+//! extern crate rustc_serialize;
+//! extern crate crypto_vault;
+//!
+//! use rustc_serialize::{Decoder, Encoder};
+//! use crypto_vault::{Vault, RawVault};
+//! use std::str::FromStr;
+//!
+//! #[derive(RustcEncodable, RustcDecodable, Debug)]
+//! struct Obj {
+//!     key: String
+//! }
+//!
+//! fn main() {
+//!     let mut vault = Vault::new().with_password("foo");
+//!     vault.objects.push(Obj { key: "bar".to_owned() });
+//!     let vault_str = vault.encrypt().unwrap().to_string();
+//!     let new_vault: Vault<Obj> = RawVault::from_str(&vault_str).unwrap().decrypt("foo").unwrap();
+//!     assert_eq!(new_vault.objects[0].key, "bar".to_owned());
+//! }
+//! ```
 #![feature(vec_push_all, plugin)]
 #![plugin(regex_macros, clippy)]
-#![deny(bad_style, unused, clippy)]
+#![deny(missing_docs, bad_style, unused, clippy)]
 
 extern crate openssl;
 extern crate rand;
@@ -13,3 +38,4 @@ mod vault;
 mod error;
 
 pub use vault::*;
+pub use error::*;
--- src/vault.rs
+++ src/vault.rs
@@ -6,11 +6,16 @@ use std::str::FromStr;
 use error::{VaultError, VResult};
 use crypto::{encrypt, decrypt, gen_bytes, derive_key};
 
+/// Stores the data in an encrypted format. The only information
+/// needed to decrypt the data should be the password.
 pub struct RawVault {
+    /// This is printed as the first line of the Vault::to_string
     pub header: String,
+    /// The raw encrypted data
+    pub data: Vec<u8>,
+
     salt: Vec<u8>,
     iv: Vec<u8>,
-    pub data: Vec<u8>
 }
 
 impl ToString for RawVault {
@@ -39,6 +44,7 @@ impl FromStr for RawVault {
 }
 
 impl RawVault {
+    /// Decrypts the data into a Vault
     pub fn decrypt<T>(&self, password: &str) -> VResult<Vault<T>> where T: Decodable + Encodable + Debug {
         let master_key = derive_key(&password, &self.salt);
         let decrypted = decrypt(&master_key, &self.iv, &self.data);
@@ -60,14 +66,19 @@ struct KeyInfo {
     salt: Vec<u8>
 }
 
+/// Stores the decrypted data. May have the master key and salt for encryption.
 #[derive(Debug)]
 pub struct Vault<T: Decodable + Encodable + Debug> {
+    /// Same as for RawVault. This is printed at the top of the encrypted store file.
     pub header: String,
+    /// The decrypted data
+    pub objects: Vec<T>,
+
     key_info: Option<KeyInfo>,
-    pub objects: Vec<T>
 }
 
 impl<T: Decodable + Encodable + Debug> Vault<T> {
+    /// Sets the password. Overrides previous password.
     pub fn set_password(&mut self, password: &str) {
         let salt = gen_bytes();
         self.key_info = Some(KeyInfo {
@@ -76,11 +87,17 @@ impl<T: Decodable + Encodable + Debug> Vault<T> {
         });
     }
 
+    /// Sets the password. Overrides previous password.
+    ///
+    /// Chainable version of set_password
     pub fn with_password(mut self, password: &str) -> Vault<T> {
         self.set_password(password);
         self
     }
 
+    /// Encrypts the store into a RawVault.
+    ///
+    /// Returns a `VaultError::NoPasswordSpecifiedError` if there is no password set on the store.
     pub fn encrypt(&self) -> VResult<RawVault> {
         let (salt, master_key) = match self.key_info {
             Some(KeyInfo { ref salt, ref master_key }) => (salt, master_key),
@@ -95,6 +112,7 @@ impl<T: Decodable + Encodable + Debug> Vault<T> {
         })
     }
 
+    /// Constructor function
     pub fn new() -> Vault<T> {
         Vault {
             header: "crypto_vault v1.0 https://github.com/zmbush/crypto_vault".to_owned(),
@@ -103,6 +121,7 @@ impl<T: Decodable + Encodable + Debug> Vault<T> {
         }
     }
 
+    /// Constructor with a custom header
     pub fn with_header<D>(h: D) -> Vault<T> where D: Display {
         Vault {
             header: format!("{}", h),
